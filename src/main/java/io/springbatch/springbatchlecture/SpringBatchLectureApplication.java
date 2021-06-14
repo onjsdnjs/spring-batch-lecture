8020bbbc93c4115a25e40777a5af7103d90104f3
onjsdnjs <onjsdnjs@gmail.com>
2021-06-14 17:04:18 +0900

    Initial commit

---
+++ src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
@@ -0,0 +1,13 @@
+package io.springbatch.springbatchlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/springbatch/springbatchlecture/SpringBatchLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatch.springbatchlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
