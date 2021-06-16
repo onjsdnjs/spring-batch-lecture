2fde6824e3383da7413e48a4e727430be9d52f9c
onjsdnjs <onjsdnjs@gmail.com>
2021-06-16 18:32:44 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
