8894b274706098ffc3230730800d8683edc87d9e
onjsdnjs <onjsdnjs@gmail.com>
2021-06-18 17:08:48 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/StartNextConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/StartNextConfiguration.java
@@ -1,13 +1,10 @@
 package io.springbatch.springbatchlecture;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class SimpleJobConfiguration {
+public class StartNextConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -52,9 +49,6 @@ public class SimpleJobConfiguration {
     public Step step3() {
         return stepBuilderFactory.get("step3")
                 .tasklet((contribution, chunkContext) -> {
-//                    throw new RuntimeException("failed");
-                    chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-                    contribution.setExitStatus(ExitStatus.STOPPED);
                     System.out.println("step3 has executed");
                     return RepeatStatus.FINISHED;
                 })
