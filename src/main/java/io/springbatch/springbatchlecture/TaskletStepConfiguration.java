2c6d0c25e454428eb0ffe92c34085de3c63bcf3b
onjsdnjs <onjsdnjs@gmail.com>
2021-06-19 17:58:21 +0900

    spring batch

--- src/main/java/io/springbatch/springbatchlecture/TaskletStepConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/TaskletStepConfiguration.java
@@ -10,15 +10,18 @@ import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.*;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Configuration
-public class StepBuilderConfiguration {
+public class TaskletStepConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -26,15 +29,14 @@ public class StepBuilderConfiguration {
     @Bean
     public Job batchJob() {
         return this.jobBuilderFactory.get("batchJob")
-                .start(step1())
-                .next(step2())
-                .next(step3())
+                .start(taskStep())
+                .next(chunkStep())
                 .build();
     }
 
     @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
+    public Step taskStep() {
+        return stepBuilderFactory.get("taskStep")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step1 has executed");
                     return RepeatStatus.FINISHED;
@@ -42,47 +44,14 @@ public class StepBuilderConfiguration {
                 .build();
     }
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
+    public Step chunkStep() {
+        return stepBuilderFactory.get("chunkStep")
                 .<String, String>chunk(3)
-                .reader(() -> null)
-                .writer(list -> {})
-                .build();
-    }
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .partitioner(step1())
+                .reader(new ListItemReader(Arrays.asList("item1","item2","item3")))
+                .writer(list -> {
+                    list.forEach(item -> System.out.println(item));
+                })
                 .build();
     }
 
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .job(job())
-                .build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .flow(flow())
-                .build();
-    }
-
-    @Bean
-    public Job job() {
-        return this.jobBuilderFactory.get("job")
-                .start(step1())
-                .next(step2())
-                .next(step3())
-                .build();
-    }
-
-    @Bean
-    public Flow flow() {
-        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
-        flowBuilder.start(step2()).end();
-        return flowBuilder.build();
-    }
 }
