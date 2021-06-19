49a83c616f9ac7c477e4ee8cc6e6785a81f2f068
onjsdnjs <onjsdnjs@gmail.com>
2021-06-19 19:59:38 +0900

    spring batch

--- src/main/java/io/springbatch/springbatchlecture/JobStepConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobStepConfiguration.java
@@ -9,8 +9,10 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.*;
 import org.springframework.batch.item.support.ListItemReader;
@@ -30,35 +32,43 @@ public class JobStepConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job batchJob() {
-        return this.jobBuilderFactory.get("batchJob")
-                .start(step1())
+    public Job parentJob() {
+        return this.jobBuilderFactory.get("parentJob")
+                .start(jobStep(null))
                 .next(step2())
                 .build();
     }
-
+    @Bean
+    public Step jobStep(JobLauncher jobLauncher) {
+        return this.stepBuilderFactory.get("jobStep")
+                .job(childJob())
+                .launcher(jobLauncher)
+                .parametersExtractor(jobParametersExtractor())
+                .build();
+    }
+    @Bean
+    public Job childJob() {
+        return this.jobBuilderFactory.get("childJob")
+                .start(step1())
+                .build();
+    }
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("stepContribution = " + stepContribution + ", chunkContext = " + chunkContext);
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("stepContribution = " + stepContribution + ", chunkContext = " + chunkContext);
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
+    }
+
+    @Bean
+    public DefaultJobParametersExtractor jobParametersExtractor() {
+        DefaultJobParametersExtractor extractor = new DefaultJobParametersExtractor();
+        extractor.setKeys(new String[]{"input.file"});
+        return extractor;
     }
 }
