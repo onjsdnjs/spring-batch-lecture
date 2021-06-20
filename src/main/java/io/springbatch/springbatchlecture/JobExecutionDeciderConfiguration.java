3d1c5ca4a4051d225b38154b5102e5cb25022f16
onjsdnjs <onjsdnjs@gmail.com>
2021-06-20 13:00:55 +0900

    spring batch

---
+++ src/main/java/io/springbatch/springbatchlecture/JobExecutionDeciderConfiguration.java
@@ -0,0 +1,79 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class JobExecutionDeciderConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(startStep())
+                .next(decider())
+                .from(decider()).on("ODD").to(oddStep())
+                .from(decider()).on("EVEN").to(evenStep())
+                .from(oddStep()).on("*").to(decider())
+                .end()
+                .build();
+    }
+    @Bean
+    public Step startStep() {
+        return stepBuilderFactory.get("startStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("This is the start tasklet");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step evenStep() {
+        return stepBuilderFactory.get("evenStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>EvenStep has executed");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step oddStep() {
+        return stepBuilderFactory.get("oddStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>OddStep has executed");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public JobExecutionDecider decider() {
+        return new CustomDecider();
+    }
+
+    public static class CustomDecider implements JobExecutionDecider {
+
+        private int count = 0;
+
+        @Override
+        public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+            count++;
+
+            if(count % 2 == 0) {
+                return new FlowExecutionStatus("EVEN");
+            }
+            else {
+                return new FlowExecutionStatus("ODD");
+            }
+        }
+    }
+}
