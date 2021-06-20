5f09d093211b65256076be59ed0b7eaaab367878
onjsdnjs <onjsdnjs@gmail.com>
2021-06-20 14:25:17 +0900

    spring batch

---
+++ src/main/java/io/springbatch/springbatchlecture/JobListener.java
@@ -0,0 +1,17 @@
+package io.springbatch.springbatchlecture;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+
+public class JobListener implements JobExecutionListener {
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        jobExecution.getExecutionContext().putString("name", "user1");
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+
+    }
+}
--- src/main/java/io/springbatch/springbatchlecture/JobScope_StepScope_Configuration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobScope_StepScope_Configuration.java
@@ -3,6 +3,7 @@ package io.springbatch.springbatchlecture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.FlowBuilder;
@@ -25,22 +26,53 @@ public class JobScope_StepScope_Configuration {
     @Bean
     public Job job() {
         return jobBuilderFactory.get("batchJob")
-                .start(step1())
+                .start(step1(null))
+                .next(step2())
+                .listener(new JobListener())
                 .build();
     }
 
     @Bean
-    public Step step1() {
+    @JobScope
+    public Step step1(@Value("#{jobExecutionContext['name']}") String name) {
+        System.out.println("jobExecutionContext['name'] : " + name);
         return stepBuilderFactory.get("step1")
-                .tasklet(tasklet(null))
+                .tasklet(tasklet1(null))
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(tasklet2(null))
+                .listener(new StepExecutionListener() {
+                    @Override
+                    public void beforeStep(StepExecution stepExecution) {
+                        stepExecution.getExecutionContext().putString("year", "2021");
+                    }
+
+                    @Override
+                    public ExitStatus afterStep(StepExecution stepExecution) {
+                        return null;
+                    }
+                })
                 .build();
     }
 
     @Bean
     @StepScope
-    public Tasklet tasklet(@Value("#{jobParameters['message']}") String message) {
+    public Tasklet tasklet1(@Value("#{jobParameters['message']}") String message) {
         return (stepContribution, chunkContext) -> {
             System.out.println(message);
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet tasklet2(@Value("#{stepExecutionContext['year']}") String year) {
+        return (stepContribution, chunkContext) -> {
+            System.out.println(year);
             return RepeatStatus.FINISHED;
         };
     }
