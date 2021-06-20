0a982b5011ea7af8e413ba1798a9c8253cc018ce
onjsdnjs <onjsdnjs@gmail.com>
2021-06-20 13:47:15 +0900

    spring batch

---
+++ src/main/java/io/springbatch/springbatchlecture/JobScope_StepScope_Configuration.java
@@ -0,0 +1,47 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class JobScope_StepScope_Configuration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(tasklet(null))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet tasklet(@Value("#{jobParameters['message']}") String message) {
+        return (stepContribution, chunkContext) -> {
+            System.out.println(message);
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
