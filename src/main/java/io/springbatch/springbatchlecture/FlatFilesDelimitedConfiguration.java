cbb4d881739c1d123e52ff75b699581e8b627d06
onjsdnjs <onjsdnjs@gmail.com>
2021-06-22 11:39:47 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/FlatFilesDelimitedConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/FlatFilesDelimitedConfiguration.java
@@ -15,18 +15,22 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class FlatFilesConfiguration {
+public class FlatFilesDelimitedConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -44,10 +48,29 @@ public class FlatFilesConfiguration {
         return stepBuilderFactory.get("step1")
                 .<String, String>chunk(3)
                 .reader(itemReader())
-                .processor(itemProcessor())
-                .writer(itemWriter())
+                .writer(new ItemWriter() {
+                    @Override
+                    public void write(List items) throws Exception {
+                        items.forEach(item -> System.out.println(item));
+                    }
+                })
                 .build();
     }
+
+    public FlatFileItemReader itemReader() {
+        return new FlatFileItemReaderBuilder<Customer>()
+                .name("flatFile")
+                .resource(new ClassPathResource("customer.csv"))
+                .lineMapper(new DefaultLineMapper<>())
+                .fieldSetMapper(new CustomerFieldSetMapper())
+                .lineTokenizer(new DelimitedLineTokenizer())
+                .targetType(Customer.class)
+                .linesToSkip(1)
+                .delimited().delimiter("|")
+                .names("name","year","age")
+                .build();
+    }
+
 
     @Bean
     public Step step2() {
