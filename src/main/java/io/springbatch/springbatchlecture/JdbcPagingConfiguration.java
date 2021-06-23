dad450d5b845a3e2294715c23420a324964254a7
onjsdnjs <onjsdnjs@gmail.com>
2021-06-23 16:32:31 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/JdbcPagingConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JdbcPagingConfiguration.java
@@ -6,10 +6,11 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.*;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.batch.item.json.JacksonJsonObjectReader;
 import org.springframework.batch.item.json.JsonItemReader;
 import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
@@ -19,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.oxm.xstream.XStreamMarshaller;
 
 import javax.persistence.EntityManagerFactory;
@@ -30,14 +32,14 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @Configuration
-public class JpaCursorConfiguration {
+public class JdbcPagingConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final EntityManagerFactory entityManagerFactory;
+    public final DataSource dataSource;
 
     @Bean
-    public Job job() {
+    public Job job() throws Exception {
         return jobBuilderFactory.get("batchJob")
                 .incrementer(new RunIdIncrementer())
                 .start(step1())
@@ -45,7 +47,7 @@ public class JpaCursorConfiguration {
     }
 
     @Bean
-    public Step step1() {
+    public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
                 .<Customer, Customer>chunk(3)
                 .reader(customItemReader())
@@ -54,19 +56,36 @@ public class JpaCursorConfiguration {
     }
 
     @Bean
-    public JpaCursorItemReader<Customer> customItemReader() {
+    public JdbcPagingItemReader<Customer> customItemReader() throws Exception {
 
         HashMap<String, Object> parameters = new HashMap<>();
         parameters.put("firstname", "A%");
 
-        return new JpaCursorItemReaderBuilder()
-                .name("jpaCursorItemReader")
-                .queryString("select c from Customer c where firstname like :firstname")
-                .entityManagerFactory(entityManagerFactory)
-                .parameterValues(parameters)
-//                .maxItemCount(10)
-//                .currentItemCount(2)
+        return new JdbcPagingItemReaderBuilder<Customer>()
+                .name("jdbcPagingItemReader")
+                .pageSize(10)
+                .fetchSize(10)
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(Customer.class))
+                .queryProvider(createQueryProvider())
+				.parameterValues(parameters)
                 .build();
+    }
+
+    @Bean
+    public PagingQueryProvider createQueryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+        queryProvider.setDataSource(dataSource); // Database에 맞는 PagingQueryProvider를 선택하기 위해
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
+		queryProvider.setWhereClause("where firstname like :firstname");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        return queryProvider.getObject();
     }
 
     @Bean
