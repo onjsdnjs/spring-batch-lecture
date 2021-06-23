aedd02a452f7764d055bde32a4738d021ff7fccd
onjsdnjs <onjsdnjs@gmail.com>
2021-06-23 14:28:02 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/XMLConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/XMLConfiguration.java
@@ -4,17 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.transform.Range;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.xstream.XStreamMarshaller;
 
-import java.util.List;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Configuration
@@ -26,37 +27,72 @@ public class XMLConfiguration {
     @Bean
     public Job job() {
         return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
-                .next(step2())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(3)
-                .reader(itemReader())
-                .writer(new ItemWriter() {
-                    @Override
-                    public void write(List items) throws Exception {
-                        items.forEach(item -> System.out.println(item));
-                    }
-                })
+                .<Customer, Customer>chunk(3)
+                .reader(customItemReader())
+                .writer(customItemWriter())
                 .build();
-    }
-
-    public FlatFileItemReader itemReader() {
-        return null;
     }
 
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 has executed");
-                    return RepeatStatus.FINISHED;
-                })
+    public StaxEventItemReader<Customer> customItemReader() {
+        return new StaxEventItemReaderBuilder<Customer>()
+                .name("xmlFileItemReader")
+                .resource(new ClassPathResource("customer.xml"))
+                .addFragmentRootElements("customer")
+                .unmarshaller(itemMarshaller())
                 .build();
+    }
+
+    /*@Bean
+    public StaxEventItemReader<Customer> customItemReader() {
+
+        XStreamMarshaller unmarshaller = new XStreamMarshaller();
+
+        Map<String, Class> aliases = new HashMap<>();
+        aliases.put("customer", Customer.class);
+        aliases.put("id", Long.class);
+        aliases.put("firstName", String.class);
+        aliases.put("lastName", String.class);
+        aliases.put("birthdate", Date.class);
+
+        unmarshaller.setAliases(aliases);
+
+        StaxEventItemReader<Customer> reader = new StaxEventItemReader<>();
+
+        reader.setResource(new ClassPathResource("customers.xml"));
+        reader.setFragmentRootElementName("customer");
+        reader.setUnmarshaller(unmarshaller);
+
+        return reader;
+    }*/
+
+    @Bean
+    public ItemWriter<Customer> customItemWriter() {
+        return items -> {
+            for (Customer item : items) {
+                System.out.println(item.toString());
+            }
+        };
+    }
+
+    @Bean
+    public XStreamMarshaller itemMarshaller() {
+        Map<String, Class<?>> aliases = new HashMap<>();
+        aliases.put("customer", Customer.class);
+        aliases.put("id", Long.class);
+        aliases.put("name", String.class);
+        aliases.put("age", Integer.class);
+        XStreamMarshaller xStreamMarshaller = new XStreamMarshaller();
+        xStreamMarshaller.setAliases(aliases);
+        return xStreamMarshaller;
     }
 }
 
