c5793aad648ea1994823194f15ca14a75a61e6f0
onjsdnjs <onjsdnjs@gmail.com>
2021-06-24 16:01:27 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/ClassifierConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/ClassifierConfiguration.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class CompositionItemConfiguration {
+public class ClassifierConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
