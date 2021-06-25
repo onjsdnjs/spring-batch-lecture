3e79d40b0d3da0a4e57dfa7cdb988dba9490df3f
onjsdnjs <onjsdnjs@gmail.com>
2021-06-25 10:25:33 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/RepeatConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/RepeatConfiguration.java
@@ -10,6 +10,11 @@ import org.springframework.batch.item.adapter.ItemWriterAdapter;
 import org.springframework.batch.item.support.ClassifierCompositeItemProcessor;
 import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder;
+import org.springframework.batch.repeat.RepeatCallback;
+import org.springframework.batch.repeat.RepeatContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
+import org.springframework.batch.repeat.support.RepeatTemplate;
 import org.springframework.classify.PatternMatchingClassifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,7 +42,7 @@ public class RepeatConfiguration {
     @Bean
     public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(10)
+                .<String, String>chunk(5)
                 .reader(new ItemReader<String>() {
                     int i = 0;
                     @Override
@@ -49,6 +54,20 @@ public class RepeatConfiguration {
                 .processor(new ItemProcessor<String, String>() {
                     @Override
                     public String process(String item) throws Exception {
+
+                        RepeatTemplate template = new RepeatTemplate();
+                        // 반복할 때마다 count 변수의 값을 1씩 증가
+                        // count 값이 chunkSize 값보다 크거나 같을 때 반복문 종료
+                        template.setCompletionPolicy(new SimpleCompletionPolicy(2));
+                        template.iterate(new RepeatCallback() {
+
+                            public RepeatStatus doInIteration(RepeatContext context) {
+                               System.out.println("repeatTest");
+                                return RepeatStatus.CONTINUABLE;
+                            }
+
+                        });
+
                         return item;
                     }
                 })
