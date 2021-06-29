2b9e0917ee5a94a0de41b66f49f13388ee2dcd38
onjsdnjs <onjsdnjs@gmail.com>
2021-06-29 14:46:40 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/ParallelStepConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/ParallelStepConfiguration.java
@@ -24,7 +24,7 @@ import java.util.*;
 
 @RequiredArgsConstructor
 @Configuration
-public class MultiThreadStepConfiguration {
+public class ParallelStepConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
