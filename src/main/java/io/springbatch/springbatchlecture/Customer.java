c936794d94b878303cee0d70754f778871e8d6bb
onjsdnjs <onjsdnjs@gmail.com>
2021-07-02 15:31:42 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/Customer.java
+++ src/main/java/io/springbatch/springbatchlecture/Customer.java
@@ -1,4 +1,17 @@
+
 package io.springbatch.springbatchlecture;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
 public class Customer {
+
+    private final long id;
+    private final String firstName;
+    private final String lastName;
+    private final Date birthdate;
 }
