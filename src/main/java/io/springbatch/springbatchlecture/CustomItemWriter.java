1c46b3f382ce65a15c6b63230a3240982fb41c07
onjsdnjs <onjsdnjs@gmail.com>
2021-07-02 11:51:00 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/CustomItemWriter.java
+++ src/main/java/io/springbatch/springbatchlecture/CustomItemWriter.java
@@ -4,10 +4,19 @@ import org.springframework.batch.item.ItemWriter;
 import java.util.List;
 
 public class CustomItemWriter implements ItemWriter<String> {
-
+    int count = 0;
     @Override
-    public void write(List<? extends String> items){
+    public void write(List<? extends String> items) throws CustomRetryException {
         for (String item : items) {
+            if(count < 2) {
+                if (count % 2 == 0) {
+                    count = count + 1;
+
+                } else if (count % 2 == 1) {
+                    count = count + 1;
+                    throw new CustomRetryException();
+                }
+            }
             System.out.println("write : " + item);
         }
     }
