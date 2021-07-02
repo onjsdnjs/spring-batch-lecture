b3010a78d9d86228bc68fab42f5c1fc88b397c5f
onjsdnjs <onjsdnjs@gmail.com>
2021-07-02 18:05:31 +0900

    spirng batch

--- src/main/java/io/springbatch/springbatchlecture/SimpleJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/SimpleJobConfiguration.java
@@ -7,11 +7,22 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Configuration
@@ -19,10 +30,10 @@ public class SimpleJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final CustomRetryListener customRetryListener;
+    private final DataSource dataSource;
 
     @Bean
-    public Job job(){
+    public Job job() throws Exception {
         return jobBuilderFactory.get("batchJob")
                 .incrementer(new RunIdIncrementer())
                 .start(step1())
@@ -30,24 +41,48 @@ public class SimpleJobConfiguration {
     }
 
     @Bean
-    public Step step1(){
+    public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
-                .<Integer, String>chunk(10)
-                .reader(listItemReader())
-                .processor(new CustomItemProcessor())
-                .writer(new CustomItemWriter())
-                .faultTolerant()
-                .retry(CustomRetryException.class)
-                .retryLimit(3)
-                .listener(customRetryListener)
+                .<Customer, Customer>chunk(100)
+                .reader(pagingItemReader())
+                .writer(customItemWriter())
+                .throttleLimit(2)
                 .build();
     }
 
     @Bean
-    public ItemReader<Integer> listItemReader() {
-        List<Integer> list = Arrays.asList(1,2,3,4);
-//        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-        return new LinkedListItemReader<>(list);
+    public JdbcPagingItemReader<Customer> pagingItemReader() {
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+
+        reader.setDataSource(this.dataSource);
+        reader.setFetchSize(300);
+        reader.setRowMapper(new CustomerRowMapper());
+
+        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        reader.setQueryProvider(queryProvider);
+
+        return reader;
+    }
+
+    @Bean
+    public JdbcBatchItemWriter customItemWriter() {
+        JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
+
+        itemWriter.setDataSource(this.dataSource);
+        itemWriter.setSql("insert into customer2 values (:id, :firstName, :lastName, :birthdate)");
+        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider());
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
     }
 }
 
