356a54e07867d1636dc78341b66064c497898f5b
onjsdnjs <onjsdnjs@gmail.com>
2021-07-09 19:32:36 +0900

    spring batch

--- src/main/java/io/springbatch/springbatchlecture/DBJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/DBJobConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class JobConfiguration {
+public class DBJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
