df9c9d666897953e405ddb3016be2f158eb0fbcb
onjsdnjs <onjsdnjs@gmail.com>
2021-07-21 20:51:51 +0900

    spring batch

--- src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -22,6 +23,7 @@ public class JobConfiguration {
     @Bean
     public Job batchJob1() {
         return this.jobBuilderFactory.get("batchJob1")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
                 .build();
--- src/main/java/io/springbatch/springbatchlecture/JobConfiguration2.java
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration2.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -22,6 +23,7 @@ public class JobConfiguration2 {
     @Bean
     public Job batchJob2() {
         return this.jobBuilderFactory.get("batchJob2")
+                .incrementer(new RunIdIncrementer())
                 .start(step3())
                 .next(step4())
                 .build();
