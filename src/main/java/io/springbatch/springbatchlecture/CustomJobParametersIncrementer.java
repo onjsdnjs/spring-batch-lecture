c31093afe8ee14b29596521f6a63b1eeb77b4f31
onjsdnjs <onjsdnjs@gmail.com>
2021-07-26 19:58:29 +0900

    spring batch

--- src/main/java/io/springbatch/springbatchlecture/CustomJobParametersIncrementer.java
+++ src/main/java/io/springbatch/springbatchlecture/CustomJobParametersIncrementer.java
@@ -11,7 +11,9 @@ public class CustomJobParametersIncrementer implements JobParametersIncrementer 
     static final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd-hhmmss");
 
     public JobParameters getNext(JobParameters parameters) {
+
         String id = format.format(new Date());
+
         return new JobParametersBuilder().addString("run.id", id).toJobParameters();
     }
 }
