94ffe05be4124bf2fd79972501594167331ab47e
onjsdnjs <onjsdnjs@gmail.com>
2021-07-29 19:33:17 +0900

    spring batch

--- src/main/java/io/springbatch/springbatchlecture/Limit_AllowConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/Limit_AllowConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -37,6 +38,7 @@ public class Limit_AllowConfiguration {
                 .build();
     }
 
+
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
@@ -47,9 +49,10 @@ public class Limit_AllowConfiguration {
                         return RepeatStatus.FINISHED;
                     }
                 })
-                .startLimit(3)
+                .allowStartIfComplete(true)
                 .build();
     }
+
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
@@ -57,10 +60,11 @@ public class Limit_AllowConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("stepContribution = " + stepContribution + ", chunkContext = " + chunkContext);
-                        return RepeatStatus.FINISHED;
+                        throw new RuntimeException("");
+//                        return RepeatStatus.FINISHED;
                     }
                 })
-                .allowStartIfComplete(true)
+                .startLimit(3)
                 .build();
     }
 }
