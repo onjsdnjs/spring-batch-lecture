aa4de491233614d717f6db5d705e8a14cb55de2a
onjsdnjs <onjsdnjs@gmail.com>
2021-08-14 07:38:25 +0900

    spring batch

--- src/main/java/io/springbatch/springbatchlecture/CustomItemProcessor.java
+++ src/main/java/io/springbatch/springbatchlecture/CustomItemProcessor.java
@@ -11,6 +11,6 @@ public class CustomItemProcessor implements ItemProcessor<Customer, Customer> {
     @Override
     public Customer process(Customer customer) throws Exception {
         customer.setName(customer.getName().toUpperCase());
-        return null;
+        return customer;
     }
 }
