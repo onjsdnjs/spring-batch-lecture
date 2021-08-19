125397638175ec36179fe103ce35bf38f0c89350
onjsdnjs <onjsdnjs@gmail.com>
2021-08-20 06:57:57 +0900

    spring batch

--- src/main/java/io/springbatch/springbatchlecture/AddressRepository.java
+++
@@ -1,6 +0,0 @@
-package io.springbatch.springbatchlecture;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- src/main/java/io/springbatch/springbatchlecture/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package io.springbatch.springbatchlecture;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
