2dc7fdb9c4ce40c4fedcbef75c94867ba2df11f3
onjsdnjs <onjsdnjs@gmail.com>
2021-08-23 19:38:49 +0900

    spring batch

--- src/main/java/io/springbatch/springbatchlecture/RepeatConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/RepeatConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.batch.repeat.RepeatCallback;
 import org.springframework.batch.repeat.RepeatContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.batch.repeat.exception.SimpleLimitExceptionHandler;
+import org.springframework.batch.repeat.policy.CompositeCompletionPolicy;
 import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
 import org.springframework.batch.repeat.policy.TimeoutTerminationPolicy;
 import org.springframework.batch.repeat.support.RepeatTemplate;
@@ -55,22 +56,33 @@ public class RepeatConfiguration {
                     }
                 })
                 .processor(new ItemProcessor<String, String>() {
+
+                    RepeatTemplate template = new RepeatTemplate();
+
                     @Override
                     public String process(String item) throws Exception {
 
-                        RepeatTemplate template = new RepeatTemplate();
                         // 반복할 때마다 count 변수의 값을 1씩 증가
                         // count 값이 chunkSize 값보다 크거나 같을 때 반복문 종료
-                        template.setCompletionPolicy(new SimpleCompletionPolicy(2));
+//                        template.setCompletionPolicy(new SimpleCompletionPolicy(2));
                         // 소요된 시간이 설정된 시간보다 클 경우 반복문 종료
-                        template.setCompletionPolicy(new TimeoutTerminationPolicy(3000));
+//                        template.setCompletionPolicy(new TimeoutTerminationPolicy(3000));
+
+                        // 여러 유형의 CompletionPolicy 를 복합적으로 처리함
+                        // 여러 개 중에 먼저 조건이 부합하는 CompletionPolicy 에 따라 반복문이 종료됨
+                        CompositeCompletionPolicy completionPolicy = new CompositeCompletionPolicy();
+                        CompletionPolicy[] completionPolicies = new CompletionPolicy[]{new TimeoutTerminationPolicy(3000),new SimpleCompletionPolicy(2)};
+                        completionPolicy.setPolicies(completionPolicies);
+                        template.setCompletionPolicy(completionPolicy);
+
                         // 예외 제한 횟수만큼 반복문 실행
-                        template.setExceptionHandler(simpleLimitExceptionHandler());
+//                        template.setExceptionHandler(simpleLimitExceptionHandler());
+
                         template.iterate(new RepeatCallback() {
 
                             public RepeatStatus doInIteration(RepeatContext context) {
                                System.out.println("repeatTest");
-//                               throw new RuntimeException("test");
+//                               throw new RuntimeException("Exception is occurred");
                                 return RepeatStatus.CONTINUABLE;
                             }
 
@@ -90,7 +102,7 @@ public class RepeatConfiguration {
 
     @Bean
     public SimpleLimitExceptionHandler simpleLimitExceptionHandler(){
-        return new SimpleLimitExceptionHandler(3);
+        return new SimpleLimitExceptionHandler(2);
     }
 }
 
