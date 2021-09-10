6e0d36b17754cdc5d702d386fcd87e67c1ff1db3
onjsdnjs <onjsdnjs@gmail.com>
2021-09-11 02:04:14 +0900

    spring batch

---
+++ src/main/java/io/springbatch/springbatchlecture/JobRegistryConfiguration.java
@@ -0,0 +1,80 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.batch.core.job.SimpleJob;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import javax.sql.DataSource;
+import java.util.*;
+
+@RequiredArgsConstructor
+@Configuration
+public class JobRegistryConfiguration {
+
+    public final JobBuilderFactory jobBuilderFactory;
+    public final StepBuilderFactory stepBuilderFactory;
+    public final JobRegistry jobRegistry;
+
+    @Bean
+    public Step step1() throws Exception {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) ->
+                        {
+                            SimpleJob job = null;
+                            for(Iterator<String> iterator = jobRegistry.getJobNames().iterator(); iterator.hasNext();){
+                                job = (SimpleJob)jobRegistry.getJob(iterator.next());
+                                System.out.println("job name: " + job.getName());
+
+                                for(Iterator<String> iterator2 = job.getStepNames().iterator(); iterator2.hasNext();) {
+                                    Step step = job.getStep(iterator2.next());
+                                    System.out.println("step name: " + step.getName());
+
+                                }
+                            }
+                            return RepeatStatus.FINISHED;
+                        }
+                )
+                .build();
+    }
+    @Bean
+    public Step step2() throws Exception {
+        return stepBuilderFactory.get("step")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .build();
+    }
+    @Bean
+    public Job job1() throws Exception {
+        return jobBuilderFactory.get("job1")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public BeanPostProcessor jobRegistryBeanPostProcessor() throws Exception {
+        JobRegistryBeanPostProcessor postProcessor = new JobRegistryBeanPostProcessor();
+        postProcessor.setJobRegistry(jobRegistry);
+        return postProcessor;
+    }
+}
+
